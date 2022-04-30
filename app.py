04ac5dd1fc42cdfb56a89890f58d2abefee0f143
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-04-30 18:17:12 +0900

    add forms

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import imp
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def root():
+    return render_template('index.html')
+
 @app.route("/home")
 def home():
-    return "welcome home"
+    return "welcome to home pages"
 
-@app.route("/base")
-def base():
-    return "Member pages"    
 
 @app.route("/success/<int:score>")
 def success(score):
@@ -18,6 +19,21 @@ def success(score):
 @app.route("/fail/<int:score>")
 def fail(score):
     return "You fail this exam "+ str(score)
+
+@app.route("/result/<int:marks>")
+def results(marks):
+    result=""
+    if marks < 50:
+        result =  'fail'
+
+    else:
+        result = 'success'
+    return redirect(url_for(result,score=marks))
+
+# submit
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
